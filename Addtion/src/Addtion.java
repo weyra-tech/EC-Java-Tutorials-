777ff03f26eb55da2e97a34da5d529fa415adf04
Atirsaw <awokeeyrw@gmail.com>
2022-09-30 14:43:00 +0300

    For addtion

---
+++ Addtion/src/Addtion.java
@@ -0,0 +1,9 @@
+public class Addtion {
+
+    public static void main(String[] args) {
+        int a=5;
+        int b=5;
+        int sum= a+b;
+        System.out.println("The sum of a and b is " + sum);
+    }
+}
