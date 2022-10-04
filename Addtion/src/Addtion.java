f4f443e78ebac47fad76cf54e377ed1f9c7edb06
Atirsaw <awokeeyrw@gmail.com>
2022-10-04 14:55:10 +0300

    Removing duplicated addtion

--- Addtion/src/Addtion.java
+++
@@ -1,9 +0,0 @@
-public class Addtion {
-
-    public static void main(String[] args) {
-        int a=5;
-        int b=5;
-        int sum= a+b;
-        System.out.println("The sum of a and b is nnnn " + sum);
-    }
-}
--- RamlaIntroduction/src/HelloRamla.java
+++ RamlaIntroduction/src/HelloRamla.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class HelloRamla {
+public class HelloRamla
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+        System.out.println("this is our first class");
+        int a=5;
+        int b=5;
+        int sum= a+b;
+        System.out.println(" the sum of the numbers a and b is  " +sum);
+
+    }
+
+
 }
